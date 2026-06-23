26fcb98d21e4a8da3f10eaef9e89416bd345202c
workalokpandey-star <work.alokpandey@gmail.com>
2026-06-23 19:22:53 +0530

    conversion of datatype

---
+++ 03_coversionoperation.js
@@ -0,0 +1,53 @@
+console.log("Conversion operation in Js");
+
+// Number Conversion
+
+// let score = "33"; // converted in number
+// let score = "33abc"; // NaN
+// let score = null; // 0
+// let score = undefined; // NaN
+// let score = true; // 1
+// let score = false; // 0
+// let score = "Alok"; // NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Number Conversion
+
+// "33" => 33
+//  "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+// "Alok" => NaN
+
+// let isLoggedIn = 1; // true
+// let isLoggedIn = 0; // false
+// let isLoggedIn = ""; // false
+// let isLoggedIn = "Alok"; // true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// Boolean Conversion
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Alok" => true
+
+
+let someNumber = 33; // convered in string
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
