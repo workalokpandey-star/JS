24a0aa7860990bdc1241539d0dfd575e5c1d6d05
workalokpandey-star <work.alokpandey@gmail.com>
2026-06-23 20:56:57 +0530

    operations in convertion

--- 03_coversionoperation.js
+++ 03_coversionoperation.js
@@ -43,11 +43,62 @@ console.log("Conversion operation in Js");
 // "Alok" => true
 
 
-let someNumber = 33; // convered in string
+// let someNumber = 33; // convered in string
 
-let stringNumber = String(someNumber);
+// let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+// *********************** Operations *******************************
+
+let value = 3
+let negVlaue = -value
+// console.log(negVlaue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Alok"
+let str2 = " Pandey"
+
+let str3 = str1 +str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true);
+// // + likhne se convertion ho jata h
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2;
+// console.table([num1, num2, num3])
+
+let gameCounter = 100;
+// gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter);
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
