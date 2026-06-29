c07784a8b297141ba23997221b4ad0a62ca559f6
workalokpandey-star <work.alokpandey@gmail.com>
2026-06-29 17:27:59 +0530

    Math and Number

---
+++ 09_number_math.js
@@ -0,0 +1,53 @@
+console.log("Numbers and Math");
+
+
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// // const otherNumber = 23.8966
+// // const otherNumber = 123.8966
+// const otherNumber = 1123.8966
+
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());//by default us value hoti h
+// console.log(hundreds.toLocaleString("en-IN")); // india ke hisab se
+
+
+// ================== Maths =================
+
+// console.log(Math);
+// console.log(Math.PI);
+
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.ceil(4.2)); // 5
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.min(4,3,6,8,2)); // 2
+// console.log(Math.max(4,3,6,8,2)); // 8
+
+
+// console.log(Math.random()); // 0 se 1 ke bich me hi aaygi
+// console.log(Math.random()*10);
+// console.log(Math.random()*10 + 1); //min 0 ni ayyegi
+// console.log((Math.random()*10) + 1); // 1 se 10 tk hi aaygi
+// console.log(Math.floor(Math.random()*10) + 1); // 1 se 10 tk hi aaygi
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
